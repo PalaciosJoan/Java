8697f86db117003a7bc44299e87af13bf94cf3f9
Joan Palacios <joan_boca_soldaty@hotmail.com>
2022-09-06 20:38:26 -0300

    Primer Commit

---
+++ src/ejercicio1_java/Ejercicio1_java.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1_java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yoansito92
+ */
+public class Ejercicio1_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    
+    int num1, num2, num3;
+    
+    Scanner Leer = new Scanner(System.in);
+    System.out.println("Ingrese dos numeros");
+    num1 = Leer.nextInt();
+    num2 = Leer.nextInt();
+    num3 = num1 + num2;
+    System.out.println("La suma de los numeros es "+num3);
+    System.out.println("Saludos");
+    
+    
+    
+    
+    
+    
+    }
+    
+}
